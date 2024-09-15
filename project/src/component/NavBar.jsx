c67cf04404d64f4ab8bcc3a61144e9829c22febd
ahmedabozied1020 <ahmedabozaied24@gmail.com>
2024-09-15 19:09:09 +0200

    add logo for logout in navbar component

--- project/src/component/NavBar.jsx
+++ project/src/component/NavBar.jsx
@@ -103,6 +103,20 @@ const NavBar = () => {
               <li>
                 <button className="focus:text-base-100 focus:bg-slate-500" onClick={logout}>
                   Logout
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+                    />
+                  </svg>
                 </button>
               </li>
             </>
